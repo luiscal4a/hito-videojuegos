908335b5b0ce8a7900f51406125a43eba522cf4a
Luis de la Cal <luiscal4a@gmail.com>
2018-10-02 11:02:54 +0200

    Subida de archivos de Unity con juego de plataformas. Gravedad salto y movimiento de personaje implementado

---
+++ Assets/CheckGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponentInParent<PlayerController>();
+	}
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 2f;
+    public float maxSpeed = 5f;
+    public bool grounded;
+    public float jumpPower = 6.5f;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    private bool jump;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            jump = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 fixedVelocity = rb2d.velocity;
+        fixedVelocity.x *= 0.75f;
+
+        if (grounded)
+        {
+            rb2d.velocity = fixedVelocity;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(Vector2.right * speed * h);
+
+        float limitedSpeed = Mathf.Clamp(rb2d.velocity.x,-maxSpeed, maxSpeed);
+        rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+       
+        if(h > 0.1f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(h < -0.1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (jump)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jump = false;
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        transform.position = new Vector3(-1, 0, 0);
+    }
+}
